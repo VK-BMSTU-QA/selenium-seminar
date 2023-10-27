25bab76322e6a963dcc93e94ddb3de4e33ba87ad
Leonid Fiksler <reddiridabl666@gmail.com>
2023-10-27 12:48:09 +0300

    Restructured code

--- Selenium/code/test_login.py
+++ Selenium/code/test_login.py
@@ -1,75 +1,11 @@
 import pytest
-from _pytest.fixtures import FixtureRequest
-
-from ui.pages.base_page import BasePage
-import ui.locators.basic_locators
-from ui.fixtures import get_driver
-
-
-class BaseCase:
-    authorize = True
-
-    @pytest.fixture(scope='function', autouse=True)
-    def setup(self, driver, config, request: FixtureRequest):
-        self.driver = driver
-        self.config = config
-
-        # self.logger = logger
-
-        self.login_page = LoginPage(driver)
-
-        if self.authorize:
-            for cookie in request.getfixturevalue('cookies'):
-                self.driver.add_cookie(cookie)
-
-            self.driver.refresh()
-
-
-@pytest.fixture(scope='session')
-def credentials():
-    with open("files/credentials") as creds:
-        return creds.readline().split()
-        
-
-@pytest.fixture(scope='session')
-def cookies(credentials, config):
-    driver = get_driver(config['browser'])
-    driver.get(config['url'])
-
-    LoginPage(driver).login(*credentials)
-    cookies = driver.get_cookies()
-
-    driver.quit()
-    return cookies
-
-
-class LoginPage(BasePage):
-    url = 'https://park.vk.company/'
-    authorize = False
-
-    locators = ui.locators.basic_locators.LoginPageLocators()
-
-    def login(self, user, password):
-        self.click(self.locators.LOGIN)
-
-        self.fill_in(self.locators.USER, user)
-        self.fill_in(self.locators.PASSWORD, password)
-
-        self.click(self.locators.SUBMIT_LOGIN)
-
-        return MainPage(self.driver)
-
-
-class MainPage(BasePage):
-    url = 'https://park.vk.company/feed/'
-
+from base import BaseCase, cookies, credentials
 
 class TestLogin(BaseCase):
-    pass
-    # authorize = False
+    authorize = False
 
-    # def test_login(self, credentials):
-    #     self.login_page.login(*credentials)
+    def test_login(self, credentials):
+        self.login_page.login(*credentials)
 
 
 class TestLK(BaseCase):
--- Selenium/code/ui/locators/basic_locators.py
+++ Selenium/code/ui/locators/basic_locators.py
@@ -2,24 +2,12 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators:
-    QUERY_LOCATOR = (By.NAME, 'q')
-    QUERY_LOCATOR_ID = (By.ID, 'id-search-field')
-    GO_BUTTON_LOCATOR = (By.XPATH, '//*[@id="submit"]')
-    START_SHELL = (By.ID, 'start-shell')
-    PYTHON_CONSOLE = (By.ID, 'hterm:row-nodes')
+    pass
 
 
 class MainPageLocators(BasePageLocators):
-    COMPREHENSIONS = (
-        By.XPATH,
-        '//code/span[@class="comment" and contains(text(), "comprehensions")]'
-    )
-    EVENTS = (By.ID, 'events')
-    READ_MORE = (By.CSS_SELECTOR, 'a.readmore')
-
-
-class EventsPageLocators(BasePageLocators):
     pass
+
 
 class LoginPageLocators(BasePageLocators):
     LOGIN = (By.LINK_TEXT, 'Войти')
