9973d2d0239d5319a7fc5cef7034ed7b37c604d7
Shagalov Viacheslav <shagalovva@student.bmstu.ru>
2023-10-29 23:17:48 +0300

    Добавил тесты на англ текст в 'О себе'

--- Selenium/code/test_settings.py
+++ Selenium/code/test_settings.py
@@ -4,7 +4,7 @@ from test_login import BaseCase
 
 
 class TestSettings(BaseCase):
-    @pytest.mark.parametrize('text', ['Это тест', ''])
+    @pytest.mark.parametrize('text', ['Это тест', '', 'About me in English.'])
     def test_about_me(self, settings_page, text):
         settings_page.edit_about(text)
         assert settings_page.get_about() == text
