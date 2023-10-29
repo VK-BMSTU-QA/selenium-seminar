4c4b8cf89db0efaaea1ca1c33d6b1f0321577ddb
Ilya <2002marsic@gmail.com>
2023-10-30 00:15:06 +0300

    fixed issues

--- Selenium/code/test_login.py
+++ Selenium/code/test_login.py
@@ -1,4 +1,5 @@
 import time
+from selenium.webdriver.support import expected_conditions as EC
 from typing import Dict
 
 import allure
@@ -8,6 +9,7 @@ from _pytest.fixtures import FixtureRequest
 from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
 
 from ui.pages.base_page import BasePage
 from ui.fixtures import driver, get_driver
@@ -37,6 +39,12 @@ class BaseCase:
 
         self.login_page = LoginPage(driver)
 
+    @pytest.fixture(scope='function')
+    def authorize(self, driver, config, cookies, request: FixtureRequest):
+        if self.authorize:
+            for cookie in cookies:
+                driver.add_cookie(cookie)
+
 
 class LoginPageLocators:
     LOGIN = (By.ID, "popup-login-form-submit")
@@ -59,6 +67,7 @@ class LoginPage(BasePage):
         self.setTextInLocatorElement(LoginPageLocators.PASSWORD_FIELD, password)
 
         submit.click()
+        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.LINK_TEXT, "Блоги")))
 
         return MainPage(self.driver)
 
@@ -81,21 +90,18 @@ class MainPage(BasePage):
     def change_widget(self, from_name, to_name):
         from_element: WebElement = self.find(main_page_widget_locator(from_name))
         from_element.click()
-        time.sleep(3)
 
         to_element: WebElement = self.find(main_page_widget_locator(to_name))
         to_element.click()
-        time.sleep(3)
 
     def navigate_to_personal_cabinet(self):
         dropdown_element: WebElement = self.find(MainPageLocators.USER_DROPDOWN)
         dropdown_element.click()
         cabinet_link: WebElement = self.find(MainPageLocators.PERSONAL_CABINET)
         cabinet_link.click()
-        time.sleep(3)
 
     @allure.step('Search')
-    def search(self, query, awaiting):
+    def search_and_assert_with_awaiting_content(self, query, awaiting):
         search_start: WebElement = self.find(MainPageLocators.SEARCH_START)
         search_start.click()
 
@@ -103,13 +109,14 @@ class MainPage(BasePage):
 
         actions = ActionChains(self.driver)
         actions.move_to_element(search_input).send_keys(query).send_keys(Keys.ENTER).perform()
-        time.sleep(3)
+        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "people-list")))
 
         self.my_assert(awaiting)
 
     @allure.step("Step 1")
     def my_assert(self, awaiting_text):
         assert awaiting_text in self.driver.page_source
+        self.driver.quit()
 
 
 class TestLogin(BaseCase):
@@ -125,29 +132,19 @@ class TestLogin(BaseCase):
 
 
 class TestLK(BaseCase):
-    def prepare_authorization(self, setup, credentials, cookies):
-        for cookie in cookies:
-            self.driver.add_cookie(cookie)
 
-    def test_lk1(self, setup, credentials, cookies):
-        self.prepare_authorization(setup, credentials, cookies)
-
+    def test_lk1(self, setup, credentials, cookies, authorize):
         main_page = MainPage(self.driver)
         main_page.change_widget('Блоги', 'Расписание')
         assert "Дисциплина" in self.driver.page_source
         self.driver.quit()
 
-    def test_lk2(self, setup, credentials, cookies):
-        self.prepare_authorization(setup, credentials, cookies)
-
+    def test_lk2(self, setup, credentials, cookies, authorize):
         main_page = MainPage(self.driver)
         main_page.navigate_to_personal_cabinet()
         assert "Активность на" in self.driver.page_source
         self.driver.quit()
 
-    def test_lk3(self, setup, credentials, cookies):
-        self.prepare_authorization(setup, credentials, cookies)
-
+    def test_lk3(self, setup, credentials, cookies, authorize):
         main_page = MainPage(self.driver)
-        main_page.search("Чернега", "Елена Владимировна")
-        self.driver.quit()
+        main_page.search_and_assert_with_awaiting_content("Чернега", "Елена Владимировна")
