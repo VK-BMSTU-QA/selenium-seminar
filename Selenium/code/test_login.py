d99e280772e9cadcf2ad649cbc2616738ef05cfb
s.d.popov <s.d.popov@vk.team>
2023-10-30 01:24:53 +0300

    add neg login test

--- Selenium/code/test_login.py
+++ Selenium/code/test_login.py
@@ -5,6 +5,15 @@ class TestLogin(BaseCase):
     authorize = False
 
     def test_login(self, credentials):
-        self.login_page.login(credentials)
+        main_page = self.login_page.login(credentials)
+
+        assert main_page.url == main_page.driver.current_url
+        
+    @pytest.mark.parametrize("invalid_creds", [{"email": "stegozavr", "password": "a"}])
+    def test_login_neg(self, invalid_creds):
+        main_page = self.login_page.login(invalid_creds)
+
+        assert main_page.url != main_page.driver.driver.current_url
+    
 
 
